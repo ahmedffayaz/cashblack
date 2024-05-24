c40cb4ed41f31fa1d3c73c5d7590b69b1a99f2d7
Ahmed Fayyaz <ahmad.fayyaz@therightsw.com>
2024-05-24 19:39:41 +0500

    Worked on homepage Ui

--- resources/views/frontend/templates/home-page-mobile-app-panel.blade.php
+++ resources/views/frontend/templates/home-page-mobile-app-panel.blade.php
@@ -1,36 +1,47 @@
-<section id="mobile-app" class="landing-section-bg landing-bg-2 section animate__animated animate__fadeInUp wow bg-gray">
-    <div class="background-image-overlay"></div>
-    <div class="container  position-relative">
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="app-img-wrap justify-content-center">
-                    <div class="img__wrap">
-                        <a href="{{ asset('pages/apps') }}">
-                            <img src="{{ asset('storage/__asset/img/home/black-phones.png') }}" alt="App iPhone"  style="width: 70%"/>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 d-flex align-items-center">
-                <div class="app-info-wrap">
-                    <div class="section-title-wrap">
-                        <h2 class="title">Cashblack Mobile <span>App</span></h2>
-                        <p class="app-font-size">
-                            With the Cashblack mobile app, you can use your Android or iOS device to browse, shop and earn cashback just as you
-                            would on your computer. Download to receive exclusive offers and promotions.
-                        </p>
-                            <a href="{{ asset('pages/apps') }}" id="apps-link" class="btn btn-primary get-cashback-btn mt-3">More Info</a>
-                    </div>
-                    <div class="mt-5">
-                        <a href="https://play.google.com/store/apps/details?id=com.cash.black.community" target="_blank">
-                            <img src="{{ asset('storage/__asset/img/play-button.png') }}" alt="Google Play" class="me-4" />
-                        </a>
-                        <a href="https://apps.apple.com/gb/app/cashblack/id1614857007" target="_blank">
-                            <img src="{{ asset('storage/__asset/img/appstore-button.png') }}" alt="App Store" />
-                        </a>
-                    </div>
+<style>
+    /* Media query for smaller screens (mobile devices) */
+    @media (max-width: 767px) {
+        .image-container .img-mobile {
+            width: 35%;
+            /* Make images take full width of the container */
+            height: auto;
+        }
+
+        .image-container .img-circle {
+            width: 55%;
+            /* Make images take full width of the container */
+            height: auto;
+        }
+
+        .right-content {
+            margin-top: 100px;
+            padding: 0 20px;
+        }
+    }
+</style>
+
+<div class="container" style="margin-bottom: 170px; width:100%; jsutify-content:center">
+    <div class="row" style="margin-top: 150px;">
+        <div class="position-relative col-md-6 text-center order-md-1 col-12 image-container container">
+            <img src="{{ asset('storage/__asset/img/home/Ellipse 9.png') }}" alt="" style="max-width: 320px;" class="img-circle">
+            <img src="{{ asset('storage/__asset/img/home/cashblack 1.png') }}" alt="" style="max-width: 240px;"
+                class="position-absolute top-50 start-50 translate-middle img-mobile">
+        </div>
+        <div class="col-md-6 right-content">
+            <div class="d-block ">
+                <h3 class="mb-3 contents "><b>Cashblack <b class="specific-color" style="color: #45a049">Mobile</b> Businesses</b> App</h3>
+                <p class="" style="font-size:large; height: auto; line-height: 28px; text-overflow: ellipsis; font-weight: 100; color: #605f5f;">With the Cashblack
+                    mobile app, you can use your
+                    Android or iOS device to browse, shop and earn cashback
+                    just as you would on your computer. Download to receive
+                    exclusive offers and promotions.</p>
+
+                <button class="btn my-4 px-5 text-white" style="background-color: #45a049;">More Info</button>
+                <div class="mt-3">
+                    <img src="{{ asset('storage/__asset/img/home/google-play-badge-logo 1.png') }}" alt="" style="max-width: 131px; max-height auto;">
+                    <img src="{{ asset('storage/__asset/img/home/download-on-the-app-store4659 1.png') }}" alt="" style="max-width: 150px;" class="ms-2">
                 </div>
             </div>
         </div>
     </div>
-</section>
+</div>
