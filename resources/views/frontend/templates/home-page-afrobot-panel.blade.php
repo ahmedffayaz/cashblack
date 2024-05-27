bcbca2ce4d7a19387e1717728a6eb6f912e496c2
Ahmed Fayyaz <ahmad.fayyaz@therightsw.com>
2024-05-27 15:16:28 +0500

    Changes in UI for Homepage before login

--- resources/views/frontend/templates/home-page-afrobot-panel.blade.php
+++ resources/views/frontend/templates/home-page-afrobot-panel.blade.php
@@ -1,26 +1,62 @@
-<section id="mobile-app" class="afrobot-section landing-section-bg landing-bg-3 section animate__animated animate__fadeInUp wow">
-    <div class="background-image-overlay"></div>
-    <div class="container position-relative">
-        <div class="row">
-            <div class="col-sm-6 d-flex align-items-center">
-                <div class="app-info-wrap">
-                    <div class="section-title-wrap">
-                        <h2 class="title">Cashblack A.F.R.O.B.O.T </h2>
-                        <p class="app-font-size">
-                            The Cashblack A.F.R.O.B.O.T is our Algorithm For Redirection Of Black-Owned Traffic. Our Chrome, Firefox and Safari web
-                            browser extension uses a bespoke algorithm and cutting edge artificial intelligence to learn how you usually shop
-                            and help you find and purchase those same goods and services at Black-owned retailers.
-                        </p>
-                        <a href="{{ asset('pages/extensions') }}" id="afrobot-link" class="btn btn-primary get-cashback-btn mt-3">More Info</a>
-                    </div>
-                </div>
+<style>
+    /* Media query for smaller screens (mobile devices) */
+    @media (max-width: 767px) {
+        .image-container .img-afrobot {
+            width: 55%;
+            height: auto;
+            padding-bottom: 100px;
+
+        }
+        .right-contents{
+            margin-top:100px;
+        }
+        .containers{
+            text-align: center;
+        }
+    }
+    .containers {
+        margin-bottom: 100px;
+    }
+
+    .rows {
+        margin-top: 170px;
+    }
+
+    .specific-color {
+        color: #45a049;
+    }
+
+    .btn {
+        background-color: #45a049;
+    }
+
+    .paragraph {
+        font-size: large;
+        height: auto;
+        line-height: 28px;
+        text-overflow: ellipsis;
+        font-weight: 100;
+        color: #605f5f;
+    }
+
+</style>
+
+<section class="section bg-white">
+    <div class="container containers">
+        <div class="row rows">
+            <div class="position-relative col-md-6 text-center image-container px-md-5 my-sm-5">
+                <img src="{{ asset('storage/__asset/img/home/Frame 49.png') }}" alt="" class="position-absolute top-50 start-50 translate-middle img-afrobot">
             </div>
-            <div class="col-sm-6">
-                <div class="app-img-wrap app-img-wrap2">
-                    <a href="{{ asset('pages/extensions') }}">
-                        <img src="{{ asset('storage/__asset/img/afrobot-logo.png') }}" alt="AFROBOT" class="img-fluid" width="450" />
-                    </a>
-                </div>
+            <div class="col-md-6 col-sm-12 right-contents mt-md-5 px-md-5">
+                <h3 class="mb-3"><b>Cashblack <b class="specific-color">A.F.R.O.B.O.T</b></h3>
+                <p class="paragraph" style="">The Cashblack A.F.R.O.B.O.T is our Algorithm For Redirection
+                    Of Black-Owned Traffic. Our Chrome, Firefox and Safari
+                    web browser extension uses a bespoke algorithm and
+                    cutting edge artificial intelligence to learn how you usually
+                    shop and help you find and purchase those same goods
+                    and services at Black-owned retailers..</p>
+
+                <button class="btn my-4 px-5 text-white">More Info</button>
             </div>
         </div>
     </div>
--- resources/views/frontend/templates/home-page-mobile-app-panel.blade.php
+++ resources/views/frontend/templates/home-page-mobile-app-panel.blade.php
@@ -3,45 +3,80 @@
     @media (max-width: 767px) {
         .image-container .img-mobile {
             width: 35%;
-            /* Make images take full width of the container */
             height: auto;
         }
 
         .image-container .img-circle {
             width: 55%;
-            /* Make images take full width of the container */
             height: auto;
         }
-
-        .right-content {
-            margin-top: 100px;
-            padding: 0 20px;
+        .containers{
+            text-align: center;
         }
     }
+
+    .img-circle {
+        max-width: 320px;
+    }
+
+    .img-mobile {
+        max-width: 240px;
+    }
+
+    .specific-color {
+        color: #45a049;
+    }
+
+    .btn {
+        background-color: #45a049;
+    }
+
+    .google-play {
+        max-width: 131px;
+        max-height: auto;
+    }
+
+    .playstore-icon {
+        max-width: 150px;
+    }
+
+    .paragraph {
+        font-size: large;
+        height: auto;
+        line-height: 28px;
+        text-overflow: ellipsis;
+        font-weight: 100;
+        color: #605f5f;
+    }
+    b{
+        font-size: 2rem;
+    }
+    .containers{
+            margin-top: 100px;
+        }
 </style>
 
-<div class="container" style="margin-bottom: 170px; width:100%; jsutify-content:center">
-    <div class="row" style="margin-top: 150px;">
-        <div class="position-relative col-md-6 text-center order-md-1 col-12 image-container container">
-            <img src="{{ asset('storage/__asset/img/home/Ellipse 9.png') }}" alt="" style="max-width: 320px;" class="img-circle">
-            <img src="{{ asset('storage/__asset/img/home/cashblack 1.png') }}" alt="" style="max-width: 240px;"
-                class="position-absolute top-50 start-50 translate-middle img-mobile">
-        </div>
-        <div class="col-md-6 right-content">
-            <div class="d-block ">
-                <h3 class="mb-3 contents "><b>Cashblack <b class="specific-color" style="color: #45a049">Mobile</b> Businesses</b> App</h3>
-                <p class="" style="font-size:large; height: auto; line-height: 28px; text-overflow: ellipsis; font-weight: 100; color: #605f5f;">With the Cashblack
+<section class="section bg-gray">
+    <div class="container containers">
+        <div class="row">
+            <div class="position-relative col-md-6 text-center order-md-1 image-container containers">
+                <img src="{{ asset('storage/__asset/img/home/Ellipse 9.png') }}" alt="" class="img-circle">
+                <img src="{{ asset('storage/__asset/img/home/cashblack 1.png') }}" alt="" class="position-absolute top-50 start-50 translate-middle img-mobile">
+            </div>
+            <div class="col-md-6 col-sm-12 right-content mt-md-5 pt-md-5">
+                <h3 class="mb-3 heading"><b>Cashblack <b class="specific-color">Mobile</b> Businesses App</b> </h3>
+                <p class="paragraph" style="">With the Cashblack
                     mobile app, you can use your
                     Android or iOS device to browse, shop and earn cashback
                     just as you would on your computer. Download to receive
                     exclusive offers and promotions.</p>
 
-                <button class="btn my-4 px-5 text-white" style="background-color: #45a049;">More Info</button>
+                <button class="btn my-4 px-5 text-white">More Info</button>
                 <div class="mt-3">
-                    <img src="{{ asset('storage/__asset/img/home/google-play-badge-logo 1.png') }}" alt="" style="max-width: 131px; max-height auto;">
-                    <img src="{{ asset('storage/__asset/img/home/download-on-the-app-store4659 1.png') }}" alt="" style="max-width: 150px;" class="ms-2">
+                    <img src="{{ asset('storage/__asset/img/home/google-play-badge-logo 1.png') }}" class="google-play" alt="">
+                    <img src="{{ asset('storage/__asset/img/home/download-on-the-app-store4659 1.png') }}" class="playstore-icon ms-2" alt="">
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
